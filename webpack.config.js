77525b75c142b1e546f56b0dfdd6c2bf1acaf7e4
jldaggs88 <daggs.jessa@gmail.com>
2020-10-30 17:15:39 -0500

    Setup front end, back end and compilier

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: "./src/index.html", 
+  filename: "./index.html"
+});
+module.exports = {
+  entry: "./src/index.js",
+  mode: 'development',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: "[name].js"
+  },
+  plugins: [
+    htmlPlugin
+  ],
+  performance: {
+    hints: false
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.s?css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        loader: "file-loader",
+        options: { name: '/static/[name].[ext]' }
+      }
+    ]
+  },
+};
