3017bcf884ace52237a7844737f2a5cd32edb1d3
jldaggs88 <daggs.jessa@gmail.com>
2020-11-01 23:29:29 -0600

    Added rounds

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import './App.css';
 const App = () => {
 
   const [questionIndex, setQuestionIndex] = useState(0);
+  const [round, setRound] = useState(1);
   const [quizComplete, setQuizComplete] = useState(false);
   const [score, setScore] = useState(0);
   const [showLanding, setShowLanding] = useState(true);
@@ -19,13 +20,21 @@ const App = () => {
       setScore(score + 1);
     }
     const nextQuestion = questionIndex + 1;
-    nextQuestion < questions.length ?
-      setQuestionIndex(nextQuestion) : setQuizComplete(true);
+    if (nextQuestion < (questions.length -1) / 2) {
+      setQuestionIndex(nextQuestion);
+      setRound(1);
+    }  else if(nextQuestion >= (questions.length -1) / 2 && nextQuestion < questions.length) {
+      setQuestionIndex(nextQuestion);
+      setRound(2);
+    } else if(nextQuestion === questions.length){
+      setQuizComplete(true);
+    }
   };
 
   const refreshPage = () => {
     setQuestionIndex(0);
     setQuizComplete(false);
+    setRound(1);
   }
 
   
@@ -43,7 +52,7 @@ const App = () => {
         <div className="outer">
           <Header />
           {!quizComplete ? <div className="prompt">
-            <QuestionCounter questionIndex={questionIndex} questions={questions} />
+            <QuestionCounter round={round} questionIndex={questionIndex} questions={questions} />
             <Question questionIndex={questionIndex} questions={questions} />
             <AnswerOptions questionIndex={questionIndex} questions={questions} handleUserChoice={handleUserChoice}/>
           </div> : <UserScore questions={questions} score={score} refreshPage={refreshPage} /> }
--- src/components/Content/QuestionCounter.jsx
+++ src/components/Content/QuestionCounter.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 
-const QuestionCounter = ( { questionIndex, questions }) => {
-
-  return (
-    <div className="count">
-      <span>Question {questionIndex + 1}</span>/{questions.length}
-    </div>
-  )
+const QuestionCounter = ( { round, questionIndex, questions }) => {
+  if(questionIndex !== 20){
+    return (
+      <div className="count">
+        <div>Round {round}! </div>
+        <span>Question {questionIndex + 1}</span>/{questions.length}
+      </div>
+    )
+  } else {
+    return (
+      <div className="count">
+        <div>Final Question!</div>
+        <span>Question {questionIndex + 1}</span>/{questions.length}
+      </div>
+    )
+  }
 };
 
 export default QuestionCounter;
