9bb5e13ddcb98108dcb59765cdaac92114631283
jldaggs88 <daggs.jessa@gmail.com>
2020-11-02 01:03:51 -0600

    added trivia memes

---
+++ src/FunStuff/images.js
@@ -0,0 +1,10 @@
+import office from './office.jpg';
+import oprah from './oprah.jpg';
+import playt from './playt.jpg';
+import tn3 from './tn3.jpg';
+import tnight from './tnight.jpg';
+import tnight2 from './tnight2.jpg';
+
+export const images = [
+  office, oprah, playt, tn3, tnight, tnight2
+];
--- src/components/Content/UserScore.jsx
+++ src/components/Content/UserScore.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import restartbtn from '../../Branding/restartbtn.png';
+import { images } from '../../FunStuff/images.js';
+
 
 const UserScore = ({ questions, score, refreshPage }) => {
 
+  let randomM; 
+
+  const renderMeme = () => {
+    randomM = images[Math.floor(Math.random() * images.length)];
+  }
+  renderMeme();
+  
   return (
     <div className="score">
       There were {questions.length} questions and you got {score} correct!
       <button className="refresh-btn" onClick={refreshPage}><img src={restartbtn} atl="" className="refresh-img" /></button>
+      <img src={randomM} alt="" className="trivia-meme"/>
     </div>
   )
 };
